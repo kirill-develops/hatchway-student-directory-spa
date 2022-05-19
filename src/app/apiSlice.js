71c1b3ec046e38546ffcd1abe7c8c1fd1397b1e9
Kirill Tchentsov <kirill.develops@gmail.com>
2022-05-19 17:33:01 -0400

    created getAllStudents endpoint

--- src/app/apiSlice.js
+++ src/app/apiSlice.js
@@ -1,11 +1,34 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+//function to create proper tags for each data point
+function providesList(resultsWithIds, tagType) {
+  return resultsWithIds
+    ? [
+      { type: tagType, id: 'LIST' },
+      ...resultsWithIds.map(({ id }) => ({ type: tagType, id })),
+    ]
+    : [{ type: tagType, id: 'LIST' }];
+}
 
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({baseUrl:''}),
+  tagTypes: ['Student'],
   endpoints: (builder) => ({
+    getAllStudents: builder.query({
+      query: () => 'https://api.hatchways.io/assessment/students',
+      transformResponse: (responseData) => responseData.students?.sort(
+        (a, b) => a.id.localeCompare(b.id),
+      ),
+      providesTags: (result) => providesList(result.ids, 'Student'),
+    }),
+    // * For Step5
+    // addTag: builder.mutation({
+    //   queryFn: (state, { studentId, tags = [] }) => {
+    //     // ToDo find student from state with studentID, if tags exsists, add to student
+    //   }
+    // })
   }),
 });
 
-export const { useGetAllStudentsQuery } = apiSlice;
+export const { useGetAllStudentsQuery, useAddTagMutation } = apiSlice;
