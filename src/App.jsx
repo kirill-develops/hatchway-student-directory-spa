c892c05ede490fb4233679486c13c52c4989c84a
Kirill Tchentsov <kirill.develops@gmail.com>
2022-05-20 12:12:29 -0400

    added searchName state used to locate all students with names containing searchName

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useGetAllStudentsQuery } from './app/apiSlice';
 
 import './styles/App.scss';
 import Student from './feature/student/Student';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   // Deconstruct variables from RTK Query generated Hook
@@ -10,18 +11,46 @@ function App() {
     data, isLoading, isSuccess, isError, error,
   } = useGetAllStudentsQuery();
 
+  // state variable to handle search value and changing it
+  const [searchName, setSearchName] = useState('');
+
+  // selecting all records from our previous data that match our search
+  const { studentSearch } = useGetAllStudentsQuery(undefined, {
+    selectFromResult: ({ data }) => ({
+      studentSearch: data?.filter((record) => {
+        const records = record.firstName.toLocaleLowerCase()
+          .startsWith(searchName.toLocaleLowerCase())
+          || record.lastName.toLocaleLowerCase()
+            .startsWith(searchName.toLocaleLowerCase());
+
+        return records;
+      }),
+    }),
+  });
+
   // create content variable to transform based on API responses
   let content;
 
   // provide content with correct data depending which hook variable is true
   if (isLoading) {
     content = <h1>Loading...</h1>;
+  } else if (studentSearch) {
+    content = studentSearch.map((student) => <Student key={student.id} studentData={student} />);
   } else if (isSuccess) {
     content = data.map((student) => <Student key={student.id} studentData={student} />);
-  } else if (isError) { content = <h1>{JSON.stringify(error, null, 2)}</h1>; }
+  } else if (isError) {
+    content = <h1>{JSON.stringify(error, null, 2)}</h1>;
+  }
 
   return (
-    <div className="App">{content}</div>
+    <div className="App">
+      <SearchBar
+        search={searchName}
+        setSearch={setSearchName}
+        placeholder="Search by name"
+      />
+      {content}
+    </div>
   );
 }
 
