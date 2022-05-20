db74d7df20b31dadea6ce55f5f178f0555c58078
Kirill Tchentsov <kirill.develops@gmail.com>
2022-05-20 13:43:33 -0400

    added button to expand student info

--- src/feature/student/Student.jsx
+++ src/feature/student/Student.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { GoPlus, GoDash } from 'react-icons/go';
 
 import Styles from './student.module.scss';
 import { Addresses } from './Address';
@@ -11,6 +12,21 @@ function Student({ studentData }) {
   } = studentData;
   const averageGrade = findAverage(grades);
 
+  const [isOpen, setIsOpen] = useReducer((prev) => !prev, false);
+
+  const icon = isOpen ? <GoDash size={54} color="rgb(127,127,127)" /> : <GoPlus size={54} color="rgb(127,127,127)" />;
+
+  const testScores = isOpen ? grades.map((grade, i) => (
+    <p>
+      Test
+      {' '}
+      {i + 1}
+      {' '}
+      {grade}
+      %
+    </p>
+  )) : '';
+
   return (
     <summary className={Styles.card}>
       <img
@@ -19,11 +35,20 @@ function Student({ studentData }) {
         className={Styles.avatar}
       />
       <content className={Styles.details}>
-        <h3 className={Styles.name}>
-          {firstName}
-          {' '}
-          {lastName}
-        </h3>
+        <header className={Styles.header}>
+          <h3 className={Styles.name}>
+            {firstName}
+            {' '}
+            {lastName}
+          </h3>
+          <button
+            type="button"
+            onClick={setIsOpen}
+            className={Styles.button}
+          >
+            {icon}
+          </button>
+        </header>
         <address>
           <Addresses addressData={email} />
         </address>
@@ -43,6 +68,7 @@ function Student({ studentData }) {
           {averageGrade}
           %
         </p>
+        {testScores}
       </content>
     </summary>
   );
